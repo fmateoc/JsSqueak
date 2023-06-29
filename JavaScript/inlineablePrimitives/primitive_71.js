b84d720421a88fe6e8c3cdf29d04fa21bc4197e1
Florin Mateoc <fmateoc@gmail.com>
2023-06-29 00:03:54 -0400

    don't raise a VM assert/debugger for normal allocation failures, just let the primitive fail

--- JavaScript/inlineablePrimitives/primitive_71.js
+++ JavaScript/inlineablePrimitives/primitive_71.js
@@ -2,5 +2,7 @@
 		const [test, result] = this.primitive_71_impl(arg);
 		if(test) return result;
 	} catch(e){
-		yield* SmalltalkVM.debug();
+		const argVal = arg.valueOf();
+		if ((argVal >>> 0) !== argVal) // if it's a positive 31 bits integer, it's a normal allocation (and thus primitive) failure
+			yield* SmalltalkVM.debug();
 	}
