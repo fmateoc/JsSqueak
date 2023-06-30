45d8aebbe5be798390293adb5c20350dc36448d1
Florin Mateoc <fmateoc@gmail.com>
2023-06-30 09:30:19 -0400

    use native/optimized Float64Array for Squeak's Float64Array, not BigInt64Array, like the generic mapping of a variableDoubleWordSubclass would imply. The Float64ArrayPlugin is not fixed yet, but at least the primitive failure code works

--- JavaScript/inlineablePrimitives/primitive_238.js
+++ JavaScript/inlineablePrimitives/primitive_238.js
@@ -1,6 +1,6 @@
     const argVal = arg.valueOf();
     if (typeof argVal === "number") {
-        const result = this.words ? this.wordsAsFloat32Array()[argVal - 1] : (this.longs ? this.longsAsFloat64Array()[argVal - 1] : undefined);
+        const result = this.words ? this.wordsAsFloat32Array()[argVal - 1] : (this.float64Array ? this.float64Array[argVal - 1] : undefined);
         if (result !== undefined)
             return Number.isSafeInteger(result) ? new Float(result) : result;
     }
--- JavaScript/inlineablePrimitives/primitive_239.js
+++ JavaScript/inlineablePrimitives/primitive_239.js
@@ -1,11 +1,14 @@
     const arg1Val = arg1.valueOf(), arg2Val = arg2.valueOf();
     if (arg1Val > 0 && (arg1Val | 0) === arg1Val && (typeof arg2Val === "number" || typeof arg2Val === "bigint")) {
-        const storage = this.words ? this.wordsAsFloat32Array() : (this.longs ? this.longsAsFloat64Array() : undefined);
+        const storage = this.words ? this.wordsAsFloat32Array() : (this.float64Array ? this.float64Array : undefined);
         if (storage) {
-            const previous = storage[arg1Val - 1], next = Number(arg2Val);
-            storage[arg1Val - 1] = next;
-            if (storage[arg1Val - 1] === next)
-                return arg2;
-            storage[arg1Val - 1] = previous;
+            const previous = storage[arg1Val - 1];
+            if (previous !== undefined) {
+                const next = Number(arg2Val);
+                storage[arg1Val - 1] = next;
+                if (storage[arg1Val - 1] === next || Number.isNaN(next) && Number.isNaN(storage[arg1Val - 1]))
+                    return arg2;
+                storage[arg1Val - 1] = previous;
+            }
         }
     }
