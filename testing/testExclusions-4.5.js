80e3bdc8d307f41729a95750b7fda2259a8e2fb7
Florin Mateoc <fmateoc@gmail.com>
2023-06-29 23:07:17 -0400

    un-exclude some test exclusions, and explain the remaining ones

--- testing/testExclusions-4.5.js
+++ testing/testExclusions-4.5.js
@@ -1,22 +1,16 @@
 SmalltalkGlobals.TestExclusions = `
-AllocationTest
-ArbitraryObjectSocketTestCase
-BitmapStreamTests
-BlockClosureTest
-BlockContextTest
-SocketStreamTest
-ClosureCompilerTest
-MethodHighlightingTests
-ContextCompilationTest
-DebuggerUnwindBug
-FlapTabTests
-ImageSegmentTest
-JPEGReadWriter2Test
-CompiledMethodComparisonTest
-DecompilerTests
-MethodContextTest
-SocketTest
-StringSocketTestCase
-TestIndenting
-Win32VMTest
+AllocationTest                      //this one crashes Chrome's JavaScript engine
+ArbitraryObjectSocketTestCase       //plugin SocketPlugin is missing
+BlockContextTest                    //no contexts support
+SocketStreamTest                    //plugin SocketPlugin is missing
+ContextCompilationTest              //no contexts support
+DebuggerUnwindBug                   //no contexts support
+FlapTabTests                        //errors out because, completely unrelated to the test subject, it modifies the bytes of a float
+ImageSegmentTest                    //no image segment primitives
+JPEGReadWriter2Test                 //plugin JPEGReadWriter2Plugin is missing
+CompiledMethodComparisonTest        //this one actually succeeds, but it doesn't test anything useful AND takes very long
+MethodContextTest                   //no contexts support
+SocketTest                          //plugin SocketPlugin is missing
+StringSocketTestCase                //plugin SocketPlugin is missing
+TestIndenting                       //fails in Squeak too
 `
--- testing/testExclusions-6.0.js
+++ testing/testExclusions-6.0.js
@@ -1,29 +1,23 @@
 SmalltalkGlobals.TestExclusions = `
-AllocationTest
-ArbitraryObjectSocketTestCase
-BitmapStreamTests
-ContextInspectorTest
-ContextVariablesInspectorTest
-SocketStreamTest
-ClosureCompilerTest
-MethodHighlightingTests
+AllocationTest                      //this one crashes Chrome's JavaScript engine
+ArbitraryObjectSocketTestCase       //plugin SocketPlugin is missing
+ContextInspectorTest                //no contexts support
+ContextVariablesInspectorTest       //no contexts support
+SocketStreamTest                    //plugin SocketPlugin is missing
 CogVMBaseImageTests
-ContextCompilationTest
-ContextTest
-DebuggerTests
-DebuggerUnwindBug
-FlapTabTests
-ImageSegmentTest
-JPEGReadWriter2Test
-BytecodeDecodingTests
-CompiledMethodComparisonTest
-DecompilerTests
-SocketTest
-SqueakSSLTest
-StringSocketTestCase
-UnicodeTest
-WebClientServerTest
+ContextCompilationTest              //no contexts support
+ContextTest                         //no contexts support
+DebuggerTests                       //no contexts support
+DebuggerUnwindBug                   //no contexts support
+FlapTabTests                        //errors out because, completely unrelated to the test subject, it modifies the bytes of a float
+ImageSegmentTest                    //no image segment primitives
+JPEGReadWriter2Test                 //plugin JPEGReadWriter2Plugin is missing
+CompiledMethodComparisonTest        //this one actually succeeds, but it doesn't test anything useful AND takes very long
+SocketTest                          //plugin SocketPlugin is missing
+SqueakSSLTest                       //plugin SqueakSSL is missing
+StringSocketTestCase                //plugin SocketPlugin is missing
+UnicodeTest                         //tries to connect to the internet
+WebClientServerTest                 //plugin SocketPlugin is missing
 WebMessageTest
-Win32VMTest
-WriteBarrierTest
+WriteBarrierTest                    //no read-only support
 `
